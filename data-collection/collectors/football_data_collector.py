cf5c766b2e1d7817c7a75796f1d25cf616398490
Anna S <>
2025-08-07 15:46:42 -0700

    Refactoring code

--- data-collection/collectors/football_data_collector.py
+++ data-collection/collectors/football_data_collector.py
@@ -11,10 +11,6 @@ additional fields: (Optional)
   - referee, attendance, venue
   - half_time_scores
   - basic_statistics (shots, possession)
-
-
-📅 Daily Sprint Plan -> Day 1 (Monday): Setup & Premier League
-https://realityai.notion.site/Sports-Scribe-21c456247e0a802085faccfd667558a0
 """
 
 import os
@@ -22,21 +18,90 @@ from datetime import datetime
 import requests
 import pandas as pd
 
-"""
-essential fields: (Must have)
-  - match_id, date, league, season
-  - home_team, away_team
-  - home_score, away_score
-  - source
 
-Field mapping between our schema and the CSV fields:
-  match_id: Referee
-  home_team: HomeTeam
-  away_team: AwayTeam
-  home_score: FTHG
-  away_score: FTAG
-  source: football-data.co.uk
-"""
+def generate_request_url(league_id, league_name, season):
+    """
+    Generate url based on league and season.
+
+    ex. Premier League 2024-2025
+        https://www.football-data.co.uk/mmz4281/2425/E0.csv
+
+    ex. Champions League 2023-2024
+        https://www.football-data.co.uk/mmz4281/2425/E1.csv
+
+    ex. La Liga 2023-2024
+        https://www.football-data.co.uk/mmz4281/2425/SP1.csv
+
+    Explanation of the URL structure:
+    https://www.football-data.co.uk/mmz4281/{season_code}/{league_code}.csv
+
+      • mmz4281 is the root folder for season data.
+      • {season_code} like 2425 refers to the 2024–2025 season.
+      • {league_code} like E0 refers to the Premier League.
+    """
+    season_code = f"{str(season)[-2:]}{str(season+1)[-2:]}"
+    base_url = "https://www.football-data.co.uk/mmz4281"
+    url = base_url + f"/{season_code}/{league_id}.csv"
+    return url
+
+
+def get_data_raw_folder():
+    # Get the cur script dir for .py
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Navigate to the data/raw, and clean up the path
+    data_raw_folder = os.path.join(cur_dir, "..", "data", "raw")
+    data_raw_folder = os.path.normpath(data_raw_folder)
+
+    return data_raw_folder
+
+
+def get_data_processed_folder():
+    # Get the cur script dir for .py
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Navigate to the data/processed, and clean up the path
+    data_processed_folder = os.path.join(cur_dir, "..", "data", "processed")
+    data_processed_folder = os.path.normpath(data_processed_folder)
+
+    return data_processed_folder
+
+
+def download_csv(url: str, filename: str = "data.csv"):
+    """
+    Fetches a CSV file from the specified URL and saves it locally.
+
+    Parameters:
+    - url (str): The URL pointing to the CSV file.
+    - filename (str): Desired name for the saved file. Defaults to 'data.csv'.
+    - overwrite (bool): Whether to overwrite existing file. Defaults to False.
+
+    Raises:
+    - ValueError: If the response status is not 200 (OK).
+    - FileExistsError: If the file exists and overwrite is False
+    """
+
+    # Build the full filepath
+    filepath = get_data_raw_folder() + filename
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        # print("Request successful:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Failed to fetch data from {url}. Error: {e}")
+
+    # Write to data/raw/filename
+    with open(filepath, "wb") as f:
+        f.write(response.content)
+    print(f"Raw CSV saved as '{filepath}'.")
+
+
+def read_csv(filename: str) -> pd.DataFrame:
+    """Read CSV file into pandas DataFrame."""
+    filepath = get_data_raw_folder() + filename
+
+    return pd.read_csv(filepath)
 
 
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
@@ -57,129 +122,99 @@ def get_columns(df, *args) -> pd.DataFrame:
     return df[valid_cols]
 
 
-def read_and_concat_csvs():
-    # Get the cur script dir for .py
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Navigate to the data/raw, and clean up the path
-    data_raw_folder = os.path.join(cur_dir, "..", "data", "raw")
-    data_raw_folder = os.path.normpath(data_raw_folder)
-
-    # Get all CSV files in the data/raw folder
-    csv_files = [f for f in os.listdir(data_raw_folder) if f.endswith(".csv")]
-    # print(csv_files)
-
-    # Read each CSV into a DataFramea, clean it and store the cleaned data to a new .csv file.
-    for file in csv_files:
-        path = os.path.join(data_raw_folder, file)
-        print(f"path: {path}")
-
-        df = pd.read_csv(path)
-
-        # Rename columns to match our schema
-        df = rename_columns(df)
-
-        df["match_id"] = df.index
-        df["league"] = "Premier League"
-        # Extract season from filename, E0_2024.csv => 2024
-        df["season"] = file.split("_")[1].split(".")[0]
-        df["source"] = "football-data.co.uk"
-
-        # Get specified columns
-        df_clean = get_columns(
-            df,
-            "match_id",
-            "date",
-            "league",
-            "season",
-            "home_team",
-            "away_team",
-            "home_score",
-            "away_score",
-            "source",
-        )
-        print("After getting essentialcolumns:")
-        print(df_clean.head())
-
-        # Ensures the folder exists without throwing an error
-        # os.makedirs("data/processed", exist_ok=True)
-
-        # Write the cleaned DataFrame to a new CSV file
-        output_file = file.replace(".csv", "") + "_cleaned.csv"
-
-        # Navigate to the data/processed, and clean up the path
-        data_processed_folder = os.path.join(cur_dir, "..", "data", "processed")
-        data_processed_folder = os.path.normpath(data_processed_folder)
-
-        output_path = os.path.join(data_processed_folder, output_file)
-
-        df_clean.to_csv(output_path, index=False)
-        print(f"Cleaned CSV saved as '{output_path}'.")
+def get_league_name_from_file(file):
+    """Extract league name from filename."""
+    # ex. E0_2024.csv => Premier-League
+    league_name = file.split("_")[0]
+    print("League name from filename: {league_name}")
+    return league_name
 
 
-def download_csv(url: str, filename: str = "data.csv", overwrite: bool = False):
-    """
-    Fetches a CSV file from the specified URL and saves it locally.
-
-    Example link to download csv data for premier league 2024-25
-    https://www.football-data.co.uk/mmz4281/2425/E0.csv
-
-
-    Explanation of the URL structure:
-    https://www.football-data.co.uk/mmz4281/{season_code}/{league_code}.csv
-
-      • mmz4281 is the root folder for season data.
-      • {season_code} like 2324 refers to the 2023–2024 season.
-      • {league_code} like E0 refers to the Premier League.
+def add_new_columns(df_clean, file):
+    """Add new columns to df_clean."""
+    df_clean["match_id"] = df_clean.index
+    df_clean["league"] = get_league_name_from_file(file)
+    df_clean["season"] = file.split("_")[1].split(".")[0]
+    df_clean["source"] = "football-data.co.uk"
+    return df_clean
 
 
-    Parameters:
-    - url (str): The URL pointing to the CSV file.
-    - filename (str): Desired name for the saved file. Defaults to 'data.csv'.
-    - overwrite (bool): Whether to overwrite existing file. Defaults to False.
-
-    Raises:
-    - ValueError: If the response status is not 200 (OK).
-    - FileExistsError: If the file exists and overwrite is False
-    """
-
-    # Build absolute path to data/raw folder
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    data_raw_folder = os.path.join(cur_dir, "..", "data", "raw")
-
-    # Alternative: Use os.path.normpath to clean up the path
-    data_raw_folder = os.path.normpath(data_raw_folder)
-
-    # Build the full filepath
-    filepath = os.path.join(data_raw_folder, filename)
-
-    if not overwrite and os.path.exists(filepath):
-        raise FileExistsError(
-            f"'{filepath}' already exists. Set overwrite=True to replace it."
-        )
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise HTTPError for bad responses
-        # print("Request successful:", response.status_code)
-    except requests.exceptions:
-        raise ValueError(f"Failed to fetch data. Status code: {response.status_code}")
-
-    # Write to data/raw/filename
-    with open(filepath, "wb") as f:
-        f.write(response.content)
-    print(f"Raw CSV saved as '{filepath}'.")
+def process_df(df):
+    # Get specified columns and reorder them
+    df_processed = get_columns(
+        df,
+        "match_id",
+        "date",
+        "league",
+        "season",
+        "home_team",
+        "away_team",
+        "home_score",
+        "away_score",
+        "source",
+    )
+    print("After getting essential columns:")
+    print(df_processed.head())
+    print("df_processed.columns:", df_processed.columns)
+    return df_processed
 
 
-"""
-if __name__ == "__main__":
-    season = 2024
-    season_code = f"{str(season)[-2:]}{str(season+1)[-2:]}"
-    league_code = "E0"  # Premier League
+def save_df_to_csv(df, raw_filename):
+    """Save DataFrame to CSV."""
 
-    base_url = "https://www.football-data.co.uk/mmz4281"
-    url = f"{base_url}/{season_code}/{league_code}.csv"
-    new_filename = f"{league_code}_{season}.csv"
+    processed_filename = raw_filename.split(".")[0] + "_processed.csv"
+    filepath = get_data_processed_folder() + processed_filename
+    df.to_csv(filepath, index=False)
+    print(f"Processed CSV saved as '{filepath}'.")
 
-    download_csv(url, new_filename, overwrite=True)
-"""
+
+# def read_and_concat_csvs():
+#     data_processed_folder = get_data_processed_folder()
+
+#     # Get all CSV files in the data/raw folder
+#     csv_files = [f for f in os.listdir(data_raw_folder) if f.endswith(".csv")]
+#     # print(csv_files)
+
+#     # Read each CSV into a DataFramea, clean it and store the cleaned data to a new .csv file.
+#     for file in csv_files:
+#         path = os.path.join(data_raw_folder, file)
+#         print(f"path: {path}")
+
+#         df = pd.read_csv(path)
+
+#         # Rename columns to match our schema
+#         df = rename_columns(df)
+
+#         # Add new columns to df
+#         df["match_id"] = df.index
+#         df["league"] = get_league_name_from_file(file)
+#         # Extract season from filename, E0_2024.csv => 2024
+#         df["season"] = file.split("_")[1].split(".")[0]
+#         df["source"] = "football-data.co.uk"
+
+#         # Ensures the folder exists without throwing an error
+#         # os.makedirs("data/processed", exist_ok=True)
+
+#         # Write the cleaned DataFrame to a new CSV file
+#         output_file = file.replace(".csv", "") + "_processed.csv"
+
+#         # Navigate to the data/processed, and clean up the path
+#         data_processed_folder = os.path.join(cur_dir, "..", "data", "processed")
+#         data_processed_folder = os.path.normpath(data_processed_folder)
+
+#         output_path = os.path.join(data_processed_folder, output_file)
+
+#         df_clean.to_csv(output_path, index=False)
+#         print(f"Cleaned CSV saved as '{output_path}'.")
+
+
+# if __name__ == "__main__":
+#     season = 2024
+#     season_code = f"{str(season)[-2:]}{str(season+1)[-2:]}"
+#     league_code = "E0"  # Premier League
+
+#     base_url = "https://www.football-data.co.uk/mmz4281"
+#     url = f"{base_url}/{season_code}/{league_code}.csv"
+#     new_filename = f"{league_code}_{season}.csv"
+
+#     download_csv(url, new_filename, overwrite=True)
--- data-collection/scripts/collect_all.py
+++ data-collection/scripts/collect_all.py
@@ -11,14 +11,6 @@ Field mapping between our schema and the CSV fields:
   away_team: AwayTeam
   home_score: FTHG
   away_score: FTAG
-
-
-example row from .csv file:
-
-Div,Date,Time,HomeTeam,AwayTeam,FTHG,FTAG,FTR,HTHG,HTAG,HTR,Referee,HS,AS,HST AST,HF,AF,HC,AC,HY,AY,HR,AR,B365H,B365D,B365A,BWH,BWD,BWA,BFH,BFD,BFA,PSH,PSD,PSA,WHH,WHD,WHA,1XBH,1XBD,1XBA,MaxH,MaxD,MaxA,AvgH,AvgD,AvgA,BFEH,BFED,BFEA,B365>2.5,B365<2.5,P>2.5,P<2.5,Max>2.5,Max<2.5,Avg>2.5,Avg<2.5,BFE>2.5,BFE<2.5,AHh,B365AHH,B365AHA,PAHH,PAHA,MaxAHH,MaxAHA,AvgAHH,AvgAHA,BFEAHH,BFEAHA,B365CH,B365CD,B365CA,BWCH,BWCD,BWCA,BFCH,BFCD,BFCA,PSCH,PSCD,PSCA,WHCH,WHCD,WHCA,1XBCH,1XBCD,1XBCA,MaxCH,MaxCD,MaxCA,AvgCH,AvgCD,AvgCA,BFECH,BFECD,BFECA,B365C>2.5,B365C<2.5,PC>2.5,PC<2.5,MaxC>2.5,MaxC<2.5,AvgC>2.5,AvgC<2.5,BFEC>2.5,BFEC<2.5,AHCh,B365CAHH,B365CAHA,PCAHH,PCAHA,MaxCAHH,MaxCAHA,AvgCAHH,AvgCAHA,BFECAHH,BFECAHA
-
-E0,16/08/2024,20:00,Man United,Fulham,1,0,H,0,0,D,R Jones,14,10,5,2,12,10,7,8,2,3,0,0,1.6,4.2,5.25,1.6,4.4,5.25,1.6,4.33,5,1.63,4.38,5.3,1.65,4.2,5,1.68,4.32,5.03,1.68,4.5,5.5,1.62,4.36,5.15,1.66,4.5,5.6,1.53,2.5,1.56,2.56,1.57,2.6,1.53,2.52,1.59,2.64,-1,2.05,1.88,2.07,1.86,2.07,1.89,2.03,1.85,2.1,1.88,1.67,4.1,5,1.65,4.2,4.8,1.62,4,5,1.65,4.23,5.28,1.6,4.2,5.5,1.66,4.15,5.33,1.7,4.33,5.5,1.66,4.2,5.02,1.72,4.2,5.4,1.62,2.3,1.63,2.38,1.66,2.45,1.61,2.37,1.68,2.46,-0.75,1.86,2.07,1.83,2.11,1.88,2.11,1.82,2.05,1.9,2.08
-
 """
 
 import sys
@@ -30,19 +22,28 @@ data_collection_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 # print("data_collection dir:", data_collection_dir)
 sys.path.append(data_collection_dir)
 
-from collectors.football_data_collector import download_csv, read_and_concat_csvs
+from collectors.football_data_collector import (
+    add_new_columns,
+    download_csv,
+    # read_and_concat_csvs,
+    generate_request_url as generate_football_data_url,
+    process_df,
+    read_csv,
+    rename_columns,
+    save_df_to_csv,
+)
 from collectors.fbref_collector import (
     convert_score_to_home_score_and_away_score,
     download_with_selenium,
     extract_columns,
     fill_and_convert_columns,
-    generate_request_url,
+    generate_request_url as generate_fbref_url,
     save_html_to_file,
     create_csv,
 )
 
-
-# Data Requirements:
+# Essential data fields:
+# Note: the fields commented out will be filled during data processing
 data_fields = [
     # "match_id",
     "date",
@@ -64,85 +65,77 @@ data_fields = [
 """
 
 
-def generate_request_url(league_id, league_name, season):
-    """
-    Generate url based on league and season.
-        ex. Premier League 2024-2025
-            "https://fbref.com/en/comps/9/2024-2025/schedule/2024-2025-Premier-League-Scores-and-Fixtures"
-
-        ex. Champions League 2023-2024
-            "https://fbref.com/en/comps/8/2024-2025/schedule/2024-2025-Champions-League-Scores-and-Fixtures"
-
-        ex. La Liga 2023-2024
-            "https://fbref.com/en/comps/12/2023-2024/schedule/2023-2024-La-Liga-Scores-and-Fixtures"
-    """
-    # league_id = 9  # Premier League
-    season_code = f"{str(season)}-{str(season+1)}"
-    # league_name = "Premier-League"  # Premier League
-    endpoint = f"{season_code}-{league_name}-Scores-and-Fixtures"
-
-    url = f"https://fbref.com/en/comps/{league_id}/{season_code}/schedule/{endpoint}"
-
-    return url
-
-
 if __name__ == "__main__":
-    # Collecting data from football-data.co.uk
+
+    print("Collecting data from football-data.co.uk...\n")
 
     # Premier League, Champions League (Le Championnat d'Europe), La Liga
-    # leagues = ["E0", "SP1", "F1"]
+    # leagues = [{"E0": "Premier-League"}, {"SP1": "La-Liga"}, {"F1": "Champions-League"}]
     # seasons = [2024, 2023, 2022, 2021, 2020]  # Last 5 seasons
-    """
-    leagues = ["F1"]
-    seasons = [2024, 2023]
 
-    for league in leagues:
-        for season in seasons:
-            season_code = f"{str(season)[-2:]}{str(season+1)[-2:]}"
-            base_url = "https://www.football-data.co.uk/mmz4281"
-            url = f"{base_url}/{season_code}/{league}.csv"
-            new_filename = f"{league}_{season}.csv"
-            download_csv(url, new_filename, overwrite=True)
-    print("\n")
-
-    read_and_concat_csvs()
-    print("\n")
-    """
-
-    """
-    # Collecting data from fbref.com
-    print(
-        "\nfbfre.com has strong bot protection, requests method is not reliable. Using Selenium method only..."
-    )
-    """
-    """
-    league_ids = [9, 8, 12]  # Premier League, Champions League, La Liga
-    league_names = ["Premier-League", "Champions-League", "La-Liga"]
-    seasons= [2020, 2021, 2022, 2023, 2024]
-    """
-    """
-    league_ids = [8]  # Premier League, Champions League, La Liga
-    league_names = ["Champions-League"]
+    leagues = [{"E0": "Premier-League"}]
     seasons = [2024]
 
-    try:
+    # Flatten the list of dictionaries, leagues
+    # ex. leagues_flat = [("E0": "Premier League"), ("SP1": "La Liga")]
+    leagues_flat = [(k, v) for league in leagues for k, v in league.items()]
+    print(
+        "Flattened leagues[]:",
+        leagues_flat,
+    )
+
+    for league_code, league_name in leagues_flat:
         for season in seasons:
-            for league_id, league_name in zip(league_ids, league_names):
-                url = generate_request_url(league_id, league_name, season)
-                print(f"\nGenerated URL: {url}")
+            url = generate_football_data_url(league_code, league_name, season)
 
-                html_content = download_with_selenium(url)
-            print("Success with Selenium method!")
+            # Download the CSV file and rename it
+            raw_csv_filename = f"{league_name}_{season}.csv"
+            download_csv(url, raw_csv_filename)
+            df_raw = read_csv(raw_csv_filename)
+            print("df_raw.head():", df_raw.head())
+            print("df_raw.columns:", df_raw.columns)
 
-            # Save the HTML content to a file for inspection
-            output_file = f"fbref_{league_name}_{season}_{season+1}.html"
-        save_html_to_file(html_content, output_file)
+            # Rename columns to match our schema
+            df_cleaned = rename_columns(df_raw)
+            df_cleaned = add_new_columns(df_cleaned, raw_csv_filename)
+            df_processed = process_df(df_cleaned)
+            df_processed = save_df_to_csv(df_processed, raw_csv_filename)
+    print("\n")
 
-    except ValueError as selenium_error:
-        print(f"\nSelenium method also failed: {selenium_error}")
-        print("\nBoth methods failed. This site has strong bot protection.")
-    """
+    # read_and_concat_csvs()
+    # print("\n")
 
+    # print("Collecting data from fbref.com...\n")
+    # print(
+    #     "\nfbfre.com has strong bot protection, requests method is not reliable. Using Selenium method only..."
+    # )
+
+    # league_ids = [9, 8, 12]  # Premier League, Champions League, La Liga
+    # league_names = ["Premier-League", "Champions-League", "La-Liga"]
+    # seasons = [2020, 2021, 2022, 2023, 2024]
+
+    # league_ids = [8]  # Premier League, Champions League, La Liga
+    # league_names = ["Champions-League"]
+    # seasons = [2024]
+
+    # try:
+    #     for season in seasons:
+    #         for league_id, league_name in zip(league_ids, league_names):
+    #             url = generate_request_url(league_id, league_name, season)
+    #             print(f"\nGenerated URL: {url}")
+
+    #             html_content = download_with_selenium(url)
+    #         print("Success with Selenium method!")
+
+    #         # Save the HTML content to a file for inspection
+    #         output_file = f"fbref_{league_name}_{season}_{season+1}.html"
+    #     save_html_to_file(html_content, output_file)
+
+    # except ValueError as selenium_error:
+    #     print(f"\nSelenium method also failed: {selenium_error}")
+    #     print("\nBoth methods failed. This site has strong bot protection.")
+
+"""
     # Add code to extract data from the downloaded .html for quick testing purpose
     # TODO: handle all 5 seasons data
     cur_dir = os.path.dirname((os.path.abspath(__file__)))
@@ -166,7 +159,6 @@ if __name__ == "__main__":
     # Assuming 'html_content' is the variable holding your downloaded HTML
     soup = BeautifulSoup(html_content, "lxml")
 
-    ##################
     # Scraping/Extracting the essential fields for DB
     df = extract_columns(soup, data_fields, html_filepath)
 
@@ -197,18 +189,4 @@ if __name__ == "__main__":
     ]
 
     create_csv(df, csv_filename)
-
-"""
-essential fields: (Must have)
-  - match_id, date, league, season
-  - home_team, away_team
-  - home_score, away_score
-  - source
-
-Field mapping between our schema and the CSV fields:
-  match_id: Referee
-  home_team: HomeTeam
-  away_team: AwayTeam
-  home_score: FTHG
-  away_score: FTAG
 """
